387d54a111de368aa10a2ab27da035fd4bdf2b68
Minseok Choe <choems@outlook.com>
2024-11-09 15:59:19 +0900

    init: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: {
+          50: 'rgb(var(--primary-50) / <alpha-value>)',
+          100: 'rgb(var(--primary-100) / <alpha-value>)',
+          200: 'rgb(var(--primary-200) / <alpha-value>)',
+          300: 'rgb(var(--primary-300) / <alpha-value>)',
+          400: 'rgb(var(--primary-400) / <alpha-value>)',
+          main: 'rgb(var(--primary-main) / <alpha-value>)',
+          600: 'rgb(var(--primary-600) / <alpha-value>)',
+          700: 'rgb(var(--primary-700) / <alpha-value>)',
+          800: 'rgb(var(--primary-800) / <alpha-value>)',
+          900: 'rgb(var(--primary-900) / <alpha-value>)',
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
