cc33cc3cff3b35d9c82289c1208ff9e9fa5a9921
Minseok Choe <choems@outlook.com>
2024-11-09 23:00:45 +0900

    remove bar layout

--- src/app/(navigated)/layout.tsx
+++ src/app/(navigated)/layout.tsx
@@ -1,11 +1,14 @@
 import type { PropsWithChildren, ReactNode } from "react";
 import Nav from "@/components/Nav";
+import Link from "next/link";
+import MyPageIcon from "public/icons/mypage.svg";
 
-
-
-export default function ForceMobileLayout({ children, bar }: PropsWithChildren<{bar : ReactNode}>) {
+export default function ForceMobileLayout({ children }: PropsWithChildren) {
   return <div className="relative flex flex-col mx-auto max-w-[648px] w-full h-full overflow-y-auto shadow-xl shadow-grey-200 bg-gradient-to-b from-primary-50 to-white">
-    {bar}
+    <header className="w-full flex flex-row justify-between px-4 py-6 items-center">
+    <Link href="/"><div className='text-3xl font-black'>🦶FootPrint</div></Link>
+    <Link href='/mypage'><div><MyPageIcon /></div></Link>
+    </header>
     <main className="flex-grow">
       {children}
     </main>
